45114bfe0a1fcaca4e6ce21677ba26419e7b6af5
Maycke <reydsonk@gmail.com>
2025-08-12 17:15:18 -0300

    Finalizando a primeira tarefa.

--- POO/Exercicio-1/login/src/user.js
+++ POO/Exercicio-1/login/src/user.js
@@ -9,16 +9,17 @@ let menu;
 let logins = [];
 
 do {
-    const registrations = logins.map((user) => {
-        return `Nome: ${user.fullName}
-        E-mail: ${user.email}
-        Senha: ${user.password}`;
+    const registrations = logins.map((user, i) => {
+            return `
+                            ${i + 1}. Nome: ${user.fullName}
+                            E-mail: ${user.email}`;
     });
 
     const registersCompleted = registrations.length === 0
         ? 'Nenhum usuário cadastrado'
         : 'Logins cadastrados:\n' + registrations.join('\n\n');
-
+        
+        console.log(logins)
     menu = parseFloat(prompt(`Por favor, escolha uma opção de acordo com seu número.
                             ${registersCompleted}
                             1. Cadastrar login.
@@ -45,12 +46,48 @@ do {
                 alert('Por favor, insira uma senha.');
                 break;
             }
+
+            function User (fullName, email, password) {
+                this.fullName = fullName;
+                this.email = email;
+                this.password = password;
+            }
+
+            const newUser = new User(fullName, email, password);
+            logins.push(newUser);
             break;
         
         case 2:
+            if (logins.length === 0) {
+                alert('Você não tem logins cadastrados no momento, cadastre alguns antes.');
+                break;
+            }
+
+            const listAcess = parseFloat(prompt(`Selecione qual e o login que deseja efetuar pelo seu número:
+                ${registersCompleted}`)) - 1;
+
+            if (isNaN(listAcess) || listAcess < 0 || listAcess >= logins.length) {
+                alert('Selecione uma opção valida.');
+                break;
+            }
+
+            let element = logins[listAcess];
+
+            const enterEmail = prompt(`Efetue login com o email do usuario ${element.fullName}.`);
+            const enterPassword = prompt(`Digite a senha do usuario ${element.fullName}`);
+
+            if (enterEmail !== element.email || enterPassword !== element.password) {
+                alert('Email ou senha incorretos, por favor tente novamente.');
+                break;
+            } else {
+                alert('Login bem sucedido!')
+            }
+            console.log(element);
+        
             break;
 
         case 3:
+            alert('Finalizando o programa.')
             break;
 
         default:
